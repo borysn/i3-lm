d183d4ec093c50022baac07eecb7b159860dcae7
borysn <borysn@gmail.com>
2017-02-01 00:38:06 -0600

    save command, begin load command

--- index.js
+++ index.js
@@ -19,20 +19,99 @@
  *
  */
 
+const exec = require('child_process').exec;
 const script = require('commander');
+const chalk = require('chalk');
 
 (function() {
 
+  // vars
+  const numWorkspaces = 4;
+
+  /**
+   * script version 
+   */
   script
     .version('1.0.0')
-    .option('-s, --save <workspace #>', 'Save layout')
-    .option('-l, --load <workspace #> <layout.json>', 'Load layout')
-    .option('-h, --help', 'show help')
-    .parse(process.argv);
 
-  console.log(script);
-  console.log(script.save);
-  console.log(script.load);
-  console.log(script.help);
+  /**
+   * save command
+   *
+   * f(workspace#)
+   */
+  script
+    .command('save <workspace>')
+    .alias('s')
+    .action((workspace) => {
+      // save
+      if (isValidWorkspace(workspace)) {
+        save(workspace);
+      } else {
+        console.log(chalk.red('[Error] invalid workspace selected'));
+        process.exit(1);
+      }
+    });
+
+  /**
+   * load command
+   *
+   * f(workspace, file)
+   */
+  script
+    .command('load <workspace> <file>')
+    .alias('l')
+    .action((workspace, file) => {
+    });
+
+  /**
+   * save
+   *
+   * assuming num is a valid workspace number
+   *
+   * save :: num => String
+   */
+  var save = (num) => {
+    // exec save command
+    exec(getSaveCmd(num), (err, stdout, stderr) => {
+      if (err) {
+        // catch error
+        console.err(err);
+      } else if (stdout) {
+        // catch stdout
+        console.log(stdout);
+      } else if (stderr) {
+        // catch stderr
+        console.err(stderr);
+      }
+    });
+  };
+
+  /**
+   * isValidWorkspace
+   *
+   * f(num)
+   *
+   * isValidWorkspace :: num -> bool
+   */
+  var isValidWorkspace = (workspace) => {
+    if (!isNaN(workspace) && workspace > 0 && workspace <= numWorkspaces) {
+      return true;
+    }
+    return false;
+  };
+
+  /**
+   * getSaveCmd
+   *
+   * getSaveCmd :: num -> String
+   */
+  var getSaveCmd = (num) => {
+   return `i3-save-tree --workspace ${num} > ~/.config/i3/layouts/workspace_${num}.json`;
+  };
+
+  /**
+   * run script
+   */
+  script.parse(process.argv);
 
 })();
