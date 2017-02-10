875d98434cfc69a10f4ac76d4d11012dc645e865
borysn <xborysn@gmail.com>
2017-02-10 11:41:41 -0600

    simple bash

--- load_layout.sh
+++ load_layout.sh
@@ -2,9 +2,9 @@
 
 i3-msg "workspace 2; append_layout /home/p3pt/.config/i3/layouts/workspace_2.json"
 
-(urxvt && ranger &)
-(cmus &)
-(cava &)
-(neofetch &)
-(clock &)
+(urxvt &)
+(urxvt &)
+(urxvt &)
+(urxvt &)
+(urxvt &)
 (urxvt &)
